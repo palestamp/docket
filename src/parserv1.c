160b27fd5d7d127630daaab5f0dd38c451d785af
Stas Kazhavets <stas.kazhavets@kufar.by>
2016-09-07 23:08:43 +0300

    try parser vmtable

--- src/parserv1.c
+++ src/parserv1.c
@@ -1,55 +1,113 @@
+#include "cfdm.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-struct parser {
+#define SCANNER_LINE_START 0
+#define SCANNER_POS_START  0
+
+struct scanner {
     size_t pos;
     size_t line;
     size_t lpos;
     char *buf;
-    
+    struct vmparser **vmt;  
 };
 
-static inline void
-unread(struct parser *p) {
-    if(p->pos > 0) {
-        p->pos--;
-        if()
-    }
+typedef int(*advancer)(struct scanner *s);
+typedef int(*parser)(char ch);
+struct vmparser {
+    advancer advancefn;
+    parser    parsefn;
+};
+
+int
+lbracket_adv(struct scanner *s) {
+    printf("LBRACKET\n");
 }
 
-static inline char
-read(struct parser *p) {
-    return *(p->buf + p->pos);
+
+void
+scanner_bind_vmt(struct scanner *s, struct vmparser vmp[]) {
+    s->vmt = &vmp;
 }
+
+void
+scanner_own_cfdm(struct scanner *s, struct cfdmap *m) {
+    s->pos = 0;
+    s->line = SCANNER_LINE_START;
+    s->lpos = SCANNER_POS_START;
+    s->buf = m->map;
+}
+
+static struct vmparser dvmt[] = {
+    {NULL, NULL}, // illegal
+    {NULL, NULL}, // eof
+    {NULL, NULL}, // cr
+    {NULL, NULL}, // lf
+    {NULL, NULL}, // ws
+
+    {NULL, NULL}, // dash
+    {&lbracket_adv, NULL}, // lbracket
+    {NULL, NULL}, // rbracket
+    {NULL, NULL}, // semicolon
+    {NULL, NULL}, // atsign
+};
 
 enum token {
     /* Special */
-    illegal,
-    eof,
-    cr,
-    lf,
-    ws,
-    
+    illegal = 0,
+    eof ,
+    cr ,
+    lf ,
+    ws ,
+
     /* Grammar */
     // Comment
-    dash,
+    dash ,
     // Possible path start
-    lbracket,
+    lbracket ,
     // Possible path end
-    rbracket,
+    rbracket ,
     // Possible path delimiter
-    semicolon,
+    semicolon ,
     // Possible title start
-    atsign,
+    atsign ,
+};
+static const char *token_lut[] = {
+    "ILLEGAL",
+    "EOF",
+    "CL",
+    "LF",
+    "WS",
+    "DASH",
+    "LBRACKET"
+    "RBRACKET" ,
+    "SEMICOLON" ,
+    "ATSIGN" ,
 };
 
 int 
-iswhitespace(ch char) {
+iswhitespace(char ch) {
     return ch == ' ' || ch == '\t';   
 }
 
-int parse()
-
-int parser_advance(struct parser *p) {
-	char ch = read(p);
-	
+int parse(struct scanner *s) {
+    char *cursor = s->buf;
+    while (*cursor++) {
+        switch (*cursor) {
+            case '[': s->vmt[lbracket]->advancefn(s); break;
+        }
+    }
+    return 0;
 }
 
+int main(int argc, const char *argv[])
+{
+    struct cfdmap *m = malloc(sizeof(m));
+    map_file("../local.docket", m);
+    struct scanner *s = malloc(sizeof(s));
+    scanner_bind_vmt(s, dvmt);
+    scanner_own_cfdm(s, m);
+    parse(s);
+    return 0;
+}
