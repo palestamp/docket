405ba973383048018f33940ab8ecef2210e16548
Stas Kazhavets <stas.kazhavets@kufar.by>
2016-09-08 09:57:01 +0300

    header recognition

--- src/parserv1.c
+++ src/parserv1.c
@@ -1,76 +1,34 @@
 #include "cfdm.h"
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #define SCANNER_LINE_START 0
 #define SCANNER_POS_START  0
+#define MAX_MORPHEME_STACK 64
 
-struct scanner {
-    size_t pos;
-    size_t line;
-    size_t lpos;
-    char *buf;
-};
+#define _SCANNER_STACK_DUMP(_s) do {        \
+    int __i;                                \
+    for(__i = 0; __i < 64; __i++) {         \
+        if(_s->tokens[__i] != 0) {                \
+            printf("%s ", token_lut[_s->tokens[__i]]);     \
+        }                                      \
+    }                                       \
+    printf("\n");                           \
+} while (0)
 
-typedef int(*advancer)(struct scanner *s);
-typedef int(*parser)(char ch);
-struct vmparser {
-    advancer advancefn;
-    parser    parsefn;
-};
+#define SCANNER_REWIND(_s) do {    \
+    _s->last_morpheme_start = -1;  \
+    _s->next_slot = 0;             \
+} while (0)                     
 
-int
-lbracket_adv(struct scanner *s) {
-    printf("LBRACKET\n");
-}
-
-int
-cl_adv(struct scanner *s) {
-    printf("CL\n");
-}
-int
-lf_adv(struct scanner *s) {
-    printf("LF\n");
-}
-int
-ws_adv(struct scanner *s) {
-    printf("WS\n");
-}
-int 
-rbracket_adv(struct scanner *s) {
-    printf("RBRACKET\n");
-}
-int 
-atsign_adv(struct scanner *s) {
-    printf("ATSIGN\n");
-}
-
-int 
-semicolon_adv(struct scanner *s) {
-    printf("SEMICOLON\n");
-}
-
-void
-scanner_own_cfdm(struct scanner *s, struct cfdmap *m) {
-    s->pos = 0;
-    s->line = SCANNER_LINE_START;
-    s->lpos = SCANNER_POS_START;
-    s->buf = (char *)m->map;
-}
-
-static struct vmparser dvmt[] = {
-    {NULL, NULL}, // illegal
-    {NULL, NULL}, // eof
-    {cl_adv, NULL}, // cr
-    {lf_adv, NULL}, // lf
-    {ws_adv, NULL}, // ws
-
-    {NULL, NULL}, // dash
-    {lbracket_adv, NULL}, // lbracket
-    {rbracket_adv, NULL}, // rbracket
-    {semicolon_adv, NULL}, // semicolon
-    {atsign_adv, NULL}, // atsign
-};
+#define SCANNER_PUT_TOKEN(_s, _token) do {  \
+    if(_s->next_slot == 0) {\
+        _s->last_morpheme_start = _s->pos;      \
+    }\
+    _s->tokens[_s->next_slot] = _token;     \
+    _s->next_slot++;                        \
+} while(0)
 
 enum token {
     /* Special */
@@ -105,10 +63,97 @@ static const char *token_lut[] = {
     "ATSIGN" ,
 };
 
-int 
-iswhitespace(char ch) {
-    return ch == ' ' || ch == '\t';   
+
+struct scanner {
+    size_t pos;
+    size_t line;
+    size_t lpos;
+    char *buf;
+    int last_morpheme_start;
+    int next_slot;
+    int tokens[MAX_MORPHEME_STACK];
+};
+
+typedef int(*advancer)(struct scanner *s);
+typedef int(*parser)(char ch);
+struct vmparser {
+    advancer advancefn;
+    parser    parsefn;
+};
+
+int
+lbracket_adv(struct scanner *s) {
+    if (s->next_slot == 1 && s->tokens[0] == lf) {
+        SCANNER_PUT_TOKEN(s, lbracket);
+    }
+    printf("LBRACKET\n");
 }
+
+int
+cl_adv(struct scanner *s) {
+    printf("CL\n");
+}
+int
+lf_adv(struct scanner *s) {
+    if(s->next_slot > 1 && s->tokens[0] == lf && s->tokens[1] == lbracket) {
+        printf("ERROR\n");
+        abort();
+    }
+    SCANNER_REWIND(s);
+    SCANNER_PUT_TOKEN(s, lf);
+    printf("LF\n");
+}
+int
+ws_adv(struct scanner *s) {
+    printf("WS\n");
+}
+int 
+rbracket_adv(struct scanner *s) {
+
+    printf("RBRACKET\n");
+    if(s->next_slot > 1 && s->tokens[0] == lf && s->tokens[1] == lbracket) {
+        printf("HEADER FOUND ");
+        // shifting all range to one char to the right
+        printf("%.*s\n", (s->pos - s->last_morpheme_start), (s->buf + s->last_morpheme_start + 1));
+        SCANNER_REWIND(s);
+    }
+}
+int 
+atsign_adv(struct scanner *s) {
+    printf("ATSIGN\n");
+}
+
+int 
+semicolon_adv(struct scanner *s) {
+    printf("SEMICOLON\n");
+}
+
+void
+scanner_own_cfdm(struct scanner *s, struct cfdmap *m) {
+    s->pos = 0;
+    s->line = SCANNER_LINE_START;
+    s->lpos = SCANNER_POS_START;
+    memset((void *)s->tokens, 0, MAX_MORPHEME_STACK * sizeof(int));
+    s->next_slot = 0;
+    s->last_morpheme_start = 0;
+    s->buf = (char *)m->map;
+}
+
+static struct vmparser dvmt[] = {
+    {NULL, NULL}, // illegal
+    {NULL, NULL}, // eof
+    {cl_adv, NULL}, // cr
+    {lf_adv, NULL}, // lf
+    {ws_adv, NULL}, // ws
+
+    {NULL, NULL}, // dash
+    {lbracket_adv, NULL}, // lbracket
+    {rbracket_adv, NULL}, // rbracket
+    {semicolon_adv, NULL}, // semicolon
+    {atsign_adv, NULL}, // atsign
+};
+
+
 
 int parse(struct scanner *s, struct vmparser vmt[]) {
     char *cursor = s->buf;
@@ -125,8 +170,9 @@ int parse(struct scanner *s, struct vmparser vmt[]) {
             case ' ': vmt[ws].advancefn(s); break;
         }
 
-
+//        _SCANNER_STACK_DUMP(s);
         cursor++;
+        s->pos++;
     }
     return 0;
 }
