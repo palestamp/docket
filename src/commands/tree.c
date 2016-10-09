b668e7738fd3f59bb0dedddaba0f77da1af37b80
palestamp <staskozhevets@gmail.com>
2016-10-09 21:34:29 +0300

    get rid off unused parameter

--- src/commands/tree.c
+++ src/commands/tree.c
@@ -10,46 +10,49 @@
 #include "trie.h"
 
 
+
+
 struct tree_padding {
     int offcursor;
     int offsets[64];
     char buf[256];
 };
 
-void
+
+static void
 print_offsets(struct tree_padding *padding) {
     int i;
-            fprintf(stderr, "[");
+    fprintf(stdout, "[");
     for(i = 0; i < padding->offcursor; i++) {
         if(i > 0) {
-            fprintf(stderr, ", ");
+            fprintf(stdout, ", ");
         }
-        fprintf(stderr, "%d", padding->offsets[i]);
+        fprintf(stdout, "%d", padding->offsets[i]);
     }
-            fprintf(stderr, "]\n");
+    fprintf(stdout, "]\n");
 }
 
-void
+static void
 trie_offset_advance(struct tree_padding *to, int len) {
     to->offsets[to->offcursor] = len;
     to->offcursor++;
 }
 
-void
+static void
 trie_offset_return(struct tree_padding *to) {
     to->offsets[to->offcursor] = 0;
     to->offcursor--;
 }
 
-void
+static void
 trie_offset_invert_last(struct tree_padding *to) {
     if(to->offcursor > 0) {
         to->offsets[to->offcursor - 1] *= -1;
     }
 }
 
-void
-print_head(const struct word_trie *trie_host, struct tree_padding *padding, int last) {
+static void
+print_head(const struct word_trie *trie_host, struct tree_padding *padding) {
     char buf[264] = "";
     if(padding->offcursor > 1) {
         int i,j;
@@ -70,12 +73,12 @@ print_head(const struct word_trie *trie_host, struct tree_padding *padding, int 
         buf[buf_pos] = '+';
         memset(buf + buf_pos + 1, '-', abs(padding->offsets[i]) - 1);
     }
-    fprintf(stderr, "%s%s\n", buf, trie_host->word);
+    fprintf(stdout, "%s%s\n", buf, trie_host->word);
 }
 
 
-void
-print_title(const struct word_trie *trie_host, struct tree_padding *padding, int last) {
+static void
+print_title(const struct word_trie *trie_host, struct tree_padding *padding) {
     char buf[264] = "";
     if(padding->offcursor > 1) {
         int i,j;
@@ -98,19 +101,19 @@ print_title(const struct word_trie *trie_host, struct tree_padding *padding, int
     TAILQ_FOREACH(leaf_inner, &trie_host->leafs, leaf) {
         struct docket *ddd = ((struct docket *)leaf_inner->data);
         char *title = docket_fix_title(ddd->title);
-        fprintf(stderr, "%s* %s (%lu)\n", buf, title, docket_get_body_len(ddd));
+        fprintf(stdout, "%s* %s (%lu)\n", buf, title, docket_get_body_len(ddd));
     }
     if (!TAILQ_EMPTY(&trie_host->leafs)) {
-        fprintf(stderr, "%s\n", buf);
+        fprintf(stdout, "%s\n", buf);
     }
 }
 
 
-void
-print_docket_node(const struct word_trie *trie_host, struct tree_padding *padding, int last) {
+static void
+print_docket_node(const struct word_trie *trie_host, struct tree_padding *padding) {
     if (trie_host->word) {
-        print_head(trie_host, padding, last);
-        print_title(trie_host, padding, last);
+        print_head(trie_host, padding);
+        print_title(trie_host, padding);
         trie_offset_advance(padding, strlen(trie_host->word));
     }
 
@@ -120,18 +123,22 @@ print_docket_node(const struct word_trie *trie_host, struct tree_padding *paddin
         // recurse into each branch except last
         for (i = 0; i < (trie_host->len - 1); i++) {
             trie_child = *(trie_host->edges + i);
-            print_docket_node(trie_child, padding, 0);
+            print_docket_node(trie_child, padding);
             trie_offset_return(padding);
         }
+
+        // recurse into last branch
         trie_child = *(trie_host->edges + i);
         trie_offset_invert_last(padding);
-        print_docket_node(trie_child, padding, 1);
+        print_docket_node(trie_child, padding);
 
         trie_offset_return(padding);
     }
 }
 
-
+/** Tree command
+ *  Output branch structure as linux tree command do
+ */
 int
 cmd_tree(int argc, const char **argv) {
     const struct option tree_options[] = {
@@ -156,7 +163,7 @@ cmd_tree(int argc, const char **argv) {
         }
         struct tree_padding padding = {.offsets = {0}, .buf = "", .offcursor = 1};
         struct docket_shelve *ds = docket_shelve_load_file((const char *)leaf->data);
-        print_docket_node(ds->trie, &padding, 0);
+        print_docket_node(ds->trie, &padding);
     }
     return 1;
 }
