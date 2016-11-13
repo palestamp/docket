f186dd25599c061beebe17e306630b3b3cebf47c
palestamp <staskozhevets@gmail.com>
2016-11-13 12:57:02 +0300

    add test for timers

---
+++ test/integration/timers_set_event_error_unrecognized_event.sh
@@ -0,0 +1,8 @@
+rm ~/.timer
+set -o verbose
+docket timer new work --abstract
+docket timer new work_child --parent work
+docket timer new work_child2 --parent work
+docket timer new work_child3 --parent work
+
+docket timer set work_child event:onstart:stopd work
